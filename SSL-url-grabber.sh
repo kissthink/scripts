d3414d9d63dad5c392ffc965e1ddc0e0ec1b3dac
Leon Teale <leonteale89@gmail.com>
2013-05-02 22:34:39 +0200

    Update SSL-url-grabber.sh

--- SSL-url-grabber.sh
+++ SSL-url-grabber.sh
@@ -15,12 +15,12 @@ echo ""
 echo "please provide some ips for the script"
 echo "   Usage: ./ssl.sh file.txt"
 echo ""
+
+#Perform nmap to only gather ips that have port 443 open else the script can hang if ran against an ip that doesnt listen on 443.
 else
 echo ""
 for ip in `cat $1`;
 do echo "$ip";
-
-#Perform nmap to only gather ips that have port 443 open else the script can hang.
 nmap $ip -p 443 -o /dev/null | egrep open; done | grep -B 1 open > temp.txt; cat temp.txt | grep '[^\.][0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}[^\.]' > ips443.txt;
 for ip in `cat ips443.txt | sort -u`; do
 
