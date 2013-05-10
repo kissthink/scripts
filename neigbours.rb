42d0f40bdc314c76adcd30bd1f84acbc95a39d78
erwanlr <erwan.lr@gmail.com>
2013-05-10 12:37:34 +0200

    New script : neighbours.rb

---
+++ neigbours.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+#
+# This script takes a domain name or an IP address and retrieve it's neighbours
+# Example: ./neighbours.rb randomstorm.com
+#
+# By: Erwan Le Rousseau (RandomStorm)
+#
+
+require 'typhoeus'
+require 'nokogiri'
+
+if address = ARGV[0]
+  api_url  = 'http://www.ipneighbour.com/'
+  response = Typhoeus.post(api_url, body: { 'domainName' => address })
+  doc      = Nokogiri::HTML(response.body)
+
+  doc.search('div#resultsContainer ul li a').each do |node|
+    puts node.text.strip
+  end
+else
+  puts 'Usage: ./neighbours.rb domain_name_or_ip'
+end
