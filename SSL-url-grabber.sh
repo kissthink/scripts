97f189c3e260cc858bd3937f3c54e03d4f66ffaa
Leon Teale <leonteale89@gmail.com>
2013-05-24 16:25:30 +0200

    Added friendly output comments

--- SSL-url-grabber.sh
+++ SSL-url-grabber.sh
@@ -18,6 +18,7 @@ echo ""
 
 #Perform nmap to only gather ips that have port 443 open else the script can hang if ran against an ip that doesnt listen on 443.
 else
+echo "Performing checks on IPs.."
 echo ""
 for ip in `cat $1`;
 do echo "$ip";
@@ -27,7 +28,8 @@ for ip in `cat ips443.txt | sort -u`; do
 #Performs the last bit of the program using curl to grab the URL
 curl -k -sL -w "%{http_code} %{url_effective}\\n" "https://$ip"; done | grep "200 https"
 fi
-
+echo ""
+echo "Scan complete"
 ## Tidy up
 rm temp.txt
 rm ips443.txt
