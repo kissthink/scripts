15ce0c44be58ed686c5965747d3cf4d26d345537
Leon Teale <leonteale89@gmail.com>
2013-06-20 17:39:26 +0200

    Added Usage checking and output

--- domain_to_ip.sh
+++ domain_to_ip.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+#Gets the ips for the specified list of domains
+#
+# Usage: ./resolves.sh urls.txt
+#
+# By: Leon Teale (RandomStorm)
+#
+
+
+## Check for correct usage
+if [ -z "$1" ];
+then
+echo ""
+echo "please provide a file of URLs"
+echo "   Usage: ./resolves.sh urls.txt"
+echo ""
+else
+echo ""
+
 for urls in `cat $1`; do
 host $urls | grep '[^\.][0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}[^\.]' | awk {'print $1 " " $4'}
 done
